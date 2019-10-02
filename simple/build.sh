f6edd63b2d21bbcf4d3cc6a802aaf1779afc2bf1
Gary Kramlich <grim@reaperworld.com>
2019-10-01 21:17:26 -0500

    Some additional updates to the subproject build image and build script

--- simple/build.sh
+++ simple/build.sh
@@ -2,6 +2,11 @@
 
 cd "${CONVEY_WORKSPACE}"
 
-BUILD_DIR="build-${DISTRO}-${VERSION}-${ARCH}"
+TARGET="${TARGET:-unknown}"
+
+BUILD_DIR="build-${TARGET}"
+
 meson "${BUILD_DIR}"
 ninja -C "${BUILD_DIR}" test
+ninja -C "${BUILD_DIR}" $(ninja -C "${BUILD_DIR}" -t targets | cut -d: -f1 | grep -E '[a-z]+-doc$')
+
